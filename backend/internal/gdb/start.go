293ce9b063e3a506f4c009ef501e9e501ae41a3c
godlev <godlev.dj@gmail.com>
2026-09-21 16:06:52 +0300

    Phase 35.8: parameterize database logs to protect secrets

--- backend/internal/gdb/start.go
+++ backend/internal/gdb/start.go
@@ -89,6 +89,7 @@ func newGormConfig() *gorm.Config {
 			SlowThreshold:             5 * time.Second,
 			LogLevel:                  logger.Warn,
 			IgnoreRecordNotFoundError: true,
+			ParameterizedQueries:      true,
 			Colorful:                  true,
 		},
 	)
