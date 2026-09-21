3703d0e8fca62af12bf37a416cea2c200496d5a7
godlev <godlev.dj@gmail.com>
2026-09-21 16:05:35 +0300

    Phase 35.8: sync UAT Swagger version

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -5477,7 +5477,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.1.0-beta.6.uat.4",
+	Version:          "0.1.0-beta.8.uat.1",
 	Host:             "",
 	BasePath:         "/api/",
 	Schemes:          []string{},
