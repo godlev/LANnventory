2f8f8a515d7487739f8a93739f6e07f3792a8d6d
godlev <godlev.dj@gmail.com>
2026-09-25 21:38:43 +0300

    Prepare v0.1.0-beta.9.uat.3 UAT

--- backend/internal/version/version.go
+++ backend/internal/version/version.go
@@ -2,4 +2,4 @@ package version
 
 // Version is the application version reported by the backend and API.
 // Release builds may override it with -ldflags "-X".
-var Version = "0.1.0-beta.9.uat.2"
+var Version = "0.1.0-beta.9.uat.3"
